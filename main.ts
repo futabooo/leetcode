b6046d64de8ce8d3cc141fa056148638a2a9830b
futabooo <takahiro.futagawa@gmail.com>
2025-08-13 23:38:39 +0900

    fix

--- main.ts
+++ main.ts
@@ -3,7 +3,10 @@ import stealthPlugin from "puppeteer-extra-plugin-stealth";
 
 (async () => {
   puppeteer.use(stealthPlugin());
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox"],
+  });
   const page = await browser.newPage();
   await page.setViewport({ width: 1280, height: 1080 });
   await page.goto("https://leetcode.com/futabooo/", {
