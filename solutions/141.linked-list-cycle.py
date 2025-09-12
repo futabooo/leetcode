0b573000fba77e0d774c80170a2d3c39e66dd67f
futabooo <takahiro.futagawa@gmail.com>
2025-09-12 22:55:35 +0900

    add solution 141

---
+++ solutions/141.linked-list-cycle.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=141 lang=python3
+#
+# [141] Linked List Cycle
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+        current = head
+        while current:
+            if current in visited:
+                return True
+            visited.add(current)
+            current = current.next
+        return False
+
+
+# @lc code=end
