7094732a06a281e242e3d9d33b43123a0e23c3a9
futabooo <takahiro.futagawa@gmail.com>
2025-11-04 21:56:56 +0900

    add solution 228

---
+++ solutions/228.summary-ranges.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=228 lang=python3
+#
+# [228] Summary Ranges
+#
+
+
+# @lc code=start
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+
+        ranges = []
+        start = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1] + 1:
+                if start == nums[i - 1]:
+                    ranges.append(str(start))
+                else:
+                    ranges.append(f"{start}->{nums[i - 1]}")
+                start = nums[i]
+
+        if start == nums[-1]:
+            ranges.append(str(start))
+        else:
+            ranges.append(f"{start}->{nums[-1]}")
+
+        return ranges
+
+
+# @lc code=end
